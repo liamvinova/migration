bc083bb65183271b3dd2de1e2b446be15646123e
Admin <admin@MacBook-Pro.local>
2020-09-03 11:51:55 +0700

    ïnit project

---
+++ MVCMigration/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MVCMigration.Infrastructure.Models;
+
+namespace MVCMigration.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MVCMigration/Infrastructure/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVCMigration.Infrastructure.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CompanyName { get; set; }
+        public string Designation { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
